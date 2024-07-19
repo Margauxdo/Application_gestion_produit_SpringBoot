47a50d124ca2a96ce72515ebc60f87d11d61d774
Margaux Doisy <margauxdoisy@gmail.com>
2024-07-19 14:32:35 +0200

    affiche la liste + controller

--- src/main/java/org/example/application_gestion_produit_springboot/controller/ProductController.java
+++ src/main/java/org/example/application_gestion_produit_springboot/controller/ProductController.java
@@ -1,4 +1,33 @@
 package org.example.application_gestion_produit_springboot.controller;
 
+
+import org.example.application_gestion_produit_springboot.model.Product;
+import org.example.application_gestion_produit_springboot.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
 public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/")
+    public String homePage(){
+        return "home";
+    }
+    @GetMapping("/list")
+    public String list(Model model){
+        List<Product> productList = productService.getAllProducts();
+        model.addAttribute("productList", productList);
+        return "list";
+    }
+
+
 }
