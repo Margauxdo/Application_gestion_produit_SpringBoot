39344d801c95f6229c42d32408e63396af10c6b9
Margaux Doisy <margauxdoisy@gmail.com>
2024-07-19 14:44:11 +0200

    page detail

--- src/main/java/org/example/application_gestion_produit_springboot/controller/ProductController.java
+++ src/main/java/org/example/application_gestion_produit_springboot/controller/ProductController.java
@@ -6,8 +6,10 @@ import org.example.application_gestion_produit_springboot.service.ProductService
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 public class ProductController {
@@ -28,6 +30,13 @@ public class ProductController {
         model.addAttribute("productList", productList);
         return "list";
     }
+    @GetMapping("/detail/{productId}")
+    public String detail(@PathVariable ("productId") UUID productId, Model model){
+        Product product = productService.getProductById(productId);
+        model.addAttribute("product", product);
+        return "detail";git
+    }
+
 
 
 }
